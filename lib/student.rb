58b610fa4412eea50a47d50d053e09e17be24907
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 21:43:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,7 @@ class Student
   def initialize(student_hash)
     student_hash.each do |attribute, value|
       self.send("#{attribute}=", value)
+    end 
   end
 
   def self.create_from_collection(students_array)
