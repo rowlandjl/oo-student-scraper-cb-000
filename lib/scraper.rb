0a3b9abe95498ec565d9bed178ab88ee824e3483
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 21:41:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,7 +33,7 @@ class Scraper
       end 
     end 
     student[:profile_quote] = profile_page.css('.profile-quote').text if profile_page.css('.profile-quote')
-    student[:bio] = profile_page.css()
+    student[:bio] = profile_page.css('div.bio-content.content-holder div.description-holder p').text 
     
     student 
   end
