43b2308ff8e51180d06fa736b6654d517e4945e6
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 21:25:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    doc.css 
+    binding.pry 
   end
 
   def self.scrape_profile_page(profile_url)
