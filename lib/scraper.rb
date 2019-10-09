8dd7932f7eb5b7446f1cd4303d29688b59fd8fa5
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 21:33:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
-    links = profile_page
+    links = profile_page.css()
   end
 
 end
